ed779eab7d9aee81d7dbbe2a8ba637acdf061aea
daniel <danielderu3@gmail.com>
2021-09-21 16:58:05 +0200

    made a class to handle the speech recog. using google search API for speech recog.

--- speech.py
+++ speech.py
@@ -1,26 +1,44 @@
 import speech_recognition
+import pyttsx3
+import time
 
-def stop():
-    print("the key word has been found")
+class Auto():
+    def __init__(self):
+        time.sleep(1)
+        while 1:
+            self.listen()
 
-
-recognizer = speech_recognition.Recognizer()
-keyword = "h"
-while True:
-    try:
-        with speech_recognition.Microphone() as mic:
-            # recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            audio = recognizer.listen(mic)
-
-            text = recognizer.recognize_sphinx(audio)
-            print(text)
-        try: 
-            if recognizer.recognize_sphinx(audio) == keyword:
-                stop()
-        except speech_recognition.RequestError:
-            print("request error")
-
-    except speech_recognition.UnknownValueError:
+    def listen(self):
         recognizer = speech_recognition.Recognizer()
+        try:
+            with speech_recognition.Microphone() as mic:
+                audio = recognizer.listen(mic)
+                text = recognizer.recognize_sphinx(audio)
+                print(text)
+                return text
+        except speech_recognition.RequestError:
+            self.speak("Sorry, I could not process your request")
+
+        except speech_recognition.UnknownValueError:
+            recognizer = speech_recognition.Recognizer()
+            self.speak("Sorry, I can't understand you")
+
+    def speak(self, text):
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
+    
+    def command(self, callback):
+       text = self.listen()
+       if text == "run":
+           return callback
+        
+
+        
+
+auto = Auto()
+
+
+    
 
 
